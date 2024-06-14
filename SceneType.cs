a5cb5fb5e510a9143d71215020f623457640ccbd
NeoXu1452 <95458801+Dankometer5k@users.noreply.github.com>
2024-06-14 07:53:05 -0400

    Add files via upload

---
+++ SceneType.cs
@@ -0,0 +1,15 @@
+
+namespace UnityCore {
+
+    namespace Scenes {
+
+        // Note: Name your SceneType enumerations just like the scene files in your Unity Scenes folder
+        // i.e. MainMenuScene, Level01Scene, etc
+        public enum SceneType {
+            None,
+            Menu,
+            Load,
+            Game,
+        }
+    }
+}
---
+++ TestScene.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+using UnityCore.Menu;
+using UnityCore.Scenes;
+
+namespace UnityCore {
+
+    namespace Scene {
+
+        public class TestScene : MonoBehaviour
+        {
+
+            public SceneController sceneController;
+
+#region Unity Functions
+#if UNITY_EDITOR
+            private void Update() {
+                if (Input.GetKeyUp(KeyCode.M)) {
+                    sceneController.Load(SceneType.Menu, (_scene) => {Debug.Log("Scene ["+_scene+"] loaded from test script!");}, false, PageType.Loading);
+                }
+                if (Input.GetKeyUp(KeyCode.G)) {
+                    sceneController.Load(SceneType.Game);
+                }
+            }
+#endif
+#endregion
+        }
+    }
+}
